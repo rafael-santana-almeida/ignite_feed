7f69e792f7d8d68fe717bbbf436dd68855ed87ad
Rafael Santana <rafaelsantana.devweb@gmail.com>
2022-07-10 18:28:56 -0300

    Add feature likes on comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,18 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
+import { useState } from 'react';
 import { Avatar } from './Avatar';
 
 import styles from './Comment.module.css';
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount(likeCount + 1);
   }
 
   return (
@@ -21,7 +28,7 @@ export function Comment({ content, onDeleteComment }) {
               <time title='12 de Junho às 20:57' dateTime='2022-06-12 20:57:00'>Cerca de 1h atrás</time>
             </div>
 
-            <button  onClick={handleDeleteComment} title='Deletar comentário'>
+            <button onClick={handleDeleteComment} title='Deletar comentário'>
               <Trash size={24} />
             </button>
           </header>
@@ -30,9 +37,9 @@ export function Comment({ content, onDeleteComment }) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -41,8 +41,6 @@ export function Post({ author, content, publishedAt}) {
       return comment !== commentToDelete;
     });
 
-    console.log({commentToDelete})
-
     setComments(commentsWithoutDeletedOne);
   }
 
