60232a0fa8b8cce6abaa8c66a7ea69f65b9b0de8
Rafael Santana <rafaelsantana.devweb@gmail.com>
2023-04-12 21:07:49 -0300

    Startup + Header component

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,12 @@
+import igniteLogo from '../../assets/ignite-logo.svg'
+
+import styles from './styles.module.css'
+
+export function Header() {
+ return (
+  <header className={styles.header}>
+    <img src={igniteLogo} alt="Ignite Feed" /> 
+    <h1>Ignite Feed</h1>
+  </header>
+ )
+}
